669b69ae4c4bf47769876e3f582d763f6bc80584
Rochak Dhakal <rochakdh@gmail.com>
2021-05-16 22:59:57 +0545

    [Added] Add strat and end node at random position

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const grids = document.querySelector('.grids')
+
 const boxSize =  25
 const columns = Math.floor(window.innerWidth / boxSize)
 const rows = 25
@@ -7,12 +8,34 @@ const rows = 25
 grids.style.setProperty("grid-template-columns", `repeat(${columns}, 1fr)`)
 grids.style.setProperty("grid-template-rows", `repeat(${rows}, 1fr)`)
 
+let box;
 
-for (var i = 0 ; i < (rows*columns); i++){
-    let box = document.createElement('div')
+for (var index = 0 ; index < (rows*columns); index++){
+    box = document.createElement('div')
+    box.id = 'node' + index
     box.style.width =  boxSize + 'px'
     box.style.height = boxSize + 'px'
     grids.appendChild(box).className = 'box'
 }
-console.log(grids.children)
+
+function randomNumberFromRange(min,max){
+    return size = parseInt(Math.random() * (max - min) + min);
+}
+
+let startNode;
+let endNode;
+startEndNodes = function (){
+    startNode = randomNumberFromRange(0,rows*columns)
+    endNode = randomNumberFromRange(0,rows*columns)
+    if (startNode === endNode){
+        endNode = randomNumberFromRange(0,rows*columns)
+    }
+    document.getElementById(`node${startNode}`).classList.add('start')
+    document.getElementById(`node${endNode}`).classList.add('end')
+}
+
+startEndNodes();
+addEventListener('click',function(e){
+    console.log(e.target.id)
+})
 
